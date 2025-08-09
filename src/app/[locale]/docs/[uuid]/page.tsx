ae62869c0cf8e2a0f33ac828dd61a494bbdfe6c4
Nikita <hello.kozloff@gmail.com>
2025-08-09 23:23:03 +0300

    update markdown styles

--- src/app/[locale]/docs/[uuid]/page.tsx
+++ src/app/[locale]/docs/[uuid]/page.tsx
@@ -1,1 +1,36 @@
-import { MarkdownEditor } from "@/entities/markdown";const content = `  # Заголовок первого уровня (h1)    ## Заголовок второго уровня (h2)    ### Заголовок третьего уровня (h3)    #### Заголовок четвёртого уровня (h4)    ##### Заголовок четвёртого уровня (h5)    ###### Заголовок четвёртого уровня (h6)    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, soluta!    I just love **bold text**.    Italicized text is the *cat's meow*.    At the command prompt, type \`nano\`.    My favorite search engine is [Duck Duck Go](https://duckduckgo.com).    ![The San Juan Mountains are beautiful!](https://mdg.imgix.net/assets/images/san-juan-mountains.jpg?auto=format&fit=clip&q=40&w=1080 "San Juan Mountains")`;export default function DocumentPage() {  return (    <div className="flex justify-center">      <MarkdownEditor        initialMarkdown={content}      />    </div>  );}
+import { MarkdownEditor } from "@/entities/markdown";
+
+const content = `
+  # Getting started
+
+  Welcome to the *Simple Editor* template! This template integrates open source UI components and Tiptap extensions licensed under MIT.
+
+  Integrate it by following the [Tiptap UI Components docs](https://duckduckgo.com "Tiptap UI Components docs") or using our CLI tool.
+
+  ## Features
+
+  > A fully responsive rich text editor with built-in support for common formatting and layout tools. Type markdown \`**\` or use keyboard shortcuts \`⌘+B\` for most all common markdown marks.
+
+  Add images, customize alignment, and apply advanced formatting to make your writing more engaging and professional.
+
+  ![The San Juan Mountains are beautiful!](https://mdg.imgix.net/assets/images/san-juan-mountains.jpg?auto=format&fit=clip&q=40&w=1080 "San Juan Mountains")
+  
+  1. Superscript (x2) and Subscript (H2O) for precision.
+  2. Typographic conversion: automatically convert to \`->\` an arrow →.
+  
+  ***
+  
+  ## Make it your own
+  
+  Switch between light and dark modes, and tailor the editor's appearance with customizable CSS to match your style.
+`;
+
+export default function DocumentPage() {
+  return (
+    <div className="max-w-2xl mx-auto p-12">
+      <MarkdownEditor
+        initialMarkdown={content}
+      />
+    </div>
+  );
+}
