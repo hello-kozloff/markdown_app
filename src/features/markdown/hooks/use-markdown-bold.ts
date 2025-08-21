590d225572fc17acaadb83a0eb6109217ffe7d42
Nikita <hello.kozloff@gmail.com>
2025-08-21 15:23:24 +0300

    decompose logic

--- src/features/markdown/hooks/use-markdown-bold.ts
+++ src/features/markdown/hooks/use-markdown-bold.ts
@@ -1,34 +1,1 @@
-import { useEditorEventCallback, useEditorState } from "@handlewithcare/react-prosemirror";
-import { toggleMark } from "prosemirror-commands";
-import { isMarkActive } from "@/shared/lib/utils";
-
-export const useMarkdownBold = () => {
-  const state = useEditorState();
-
-  const toggleBold =
-    useEditorEventCallback((view) => {
-      const toggleBoldMark = toggleMark(
-        view.state.schema.marks[
-          "strong"
-        ]
-      );
-
-      toggleBoldMark(
-        view.state,
-        view.dispatch,
-        view
-      );
-
-      view.focus();
-    });
-
-  const isBold = isMarkActive(
-    state.schema.marks["strong"],
-    state
-  );
-
-  return {
-    isBold,
-    toggleBold
-  };
-};
+import {  useEditorEventCallback,  useEditorState} from "@handlewithcare/react-prosemirror";import { toggleMark } from "prosemirror-commands";import { isMarkActive } from "@/shared/lib/utils";export const useMarkdownBold = () => {  const state = useEditorState();  const toggleBold =    useEditorEventCallback((view) => {      const toggleBoldMark = toggleMark(        view.state.schema.marks[          "strong"        ]      );      toggleBoldMark(        view.state,        view.dispatch,        view      );      view.focus();    });  const isBold = isMarkActive(    state.schema.marks["strong"],    state  );  return {    isBold,    toggleBold  };};
