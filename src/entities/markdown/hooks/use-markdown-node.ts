c5f6d927ace5e8e287209dd3e5f125d9404ec326
Nikita <hello.kozloff@gmail.com>
2025-08-21 22:05:08 +0300

    support markdown node change

--- src/entities/markdown/hooks/use-markdown-node.ts
+++ src/entities/markdown/hooks/use-markdown-node.ts
@@ -1,42 +1,1 @@
-import { useCallback } from "react";
-import { useEditorEventCallback, useEditorState } from "@handlewithcare/react-prosemirror";
-import { isNodeActive } from "@/shared/lib/utils";
-import { setBlockType } from "prosemirror-commands";
-
-export interface UseMarkdownNodeOptions {
-  node: string;
-}
-
-export function useMarkdownNode({
-  node
-}: UseMarkdownNodeOptions) {
-  const state = useEditorState();
-
-  const isActive = useCallback(
-    (
-      attrs?: Record<
-        string,
-        string | number
-      >
-    ) =>
-      isNodeActive(state, node, attrs),
-    [node, state]
-  );
-
-  const setNode =
-    useEditorEventCallback(
-      (
-        view,
-        attrs?: Record<
-          string,
-          string | number
-        >
-      ) =>
-        setBlockType(
-          view.state.schema.nodes[node],
-          attrs
-        )(view.state, view.dispatch)
-    );
-
-  return { isActive, setNode };
-}
+import { useCallback } from "react";import {  useEditorEventCallback,  useEditorState} from "@handlewithcare/react-prosemirror";import { isNodeActive } from "@/shared/lib/utils";import { setBlockType } from "prosemirror-commands";export interface UseMarkdownNodeOptions {  node: string;}export function useMarkdownNode({  node}: UseMarkdownNodeOptions) {  const state = useEditorState();  const isActive = useCallback(    (      attrs?: Record<        string,        string | number      >    ) =>      isNodeActive(state, node, attrs),    [node, state]  );  const setNode =    useEditorEventCallback(      (        view,        attrs?: Record<          string,          string | number        >      ) =>        setBlockType(          view.state.schema.nodes[node],          attrs        )(view.state, view.dispatch)    );  return { isActive, setNode };}
