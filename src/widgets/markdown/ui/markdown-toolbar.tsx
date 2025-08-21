5547a28a1d0c790c7b512b8bba0d7f4696b73985
Nikita <hello.kozloff@gmail.com>
2025-08-21 13:02:54 +0300

    update

--- src/widgets/markdown/ui/markdown-toolbar.tsx
+++ src/widgets/markdown/ui/markdown-toolbar.tsx
@@ -1,56 +1,1 @@
-import {
-  MarkdownAlignCenter,
-  MarkdownAlignJustify,
-  MarkdownAlignLeft,
-  MarkdownAlignRight,
-  MarkdownBlockquote,
-  MarkdownBold,
-  MarkdownCodeBlock,
-  MarkdownCodeInline,
-  MarkdownHeading,
-  MarkdownHighlight,
-  MarkdownImage,
-  MarkdownItalic,
-  MarkdownLink,
-  MarkdownList,
-  MarkdownRedo,
-  MarkdownStrike,
-  MarkdownUnderline,
-  MarkdownUndo
-} from "@/features/markdown";
-import { Toolbar, ToolbarBlock } from "@/shared/ui/toolbar";
-
-export function MarkdownToolbar() {
-  return (
-    <div className="flex justify-center">
-      <Toolbar>
-        <ToolbarBlock>
-          <MarkdownUndo />
-          <MarkdownRedo />
-        </ToolbarBlock>
-        <ToolbarBlock>
-          <MarkdownHeading />
-          <MarkdownList />
-          <MarkdownBlockquote />
-          <MarkdownCodeBlock />
-          <MarkdownImage />
-        </ToolbarBlock>
-        <ToolbarBlock>
-          <MarkdownBold />
-          <MarkdownItalic />
-          <MarkdownUnderline />
-          <MarkdownStrike />
-          <MarkdownCodeInline />
-          <MarkdownHighlight />
-          <MarkdownLink />
-        </ToolbarBlock>
-        <ToolbarBlock>
-          <MarkdownAlignLeft />
-          <MarkdownAlignCenter />
-          <MarkdownAlignRight />
-          <MarkdownAlignJustify />
-        </ToolbarBlock>
-      </Toolbar>
-    </div>
-  );
-}
+import {  MarkdownAlignCenter,  MarkdownAlignJustify,  MarkdownAlignLeft,  MarkdownAlignRight,  MarkdownBlockquote,  MarkdownBold,  MarkdownCodeBlock,  MarkdownCodeInline,  MarkdownHeading,  MarkdownHighlight,  MarkdownImage,  MarkdownItalic,  MarkdownLink,  MarkdownList,  MarkdownRedo,  MarkdownStrike,  MarkdownUnderline,  MarkdownUndo} from "@/features/markdown";import {  Toolbar,  ToolbarBlock} from "@/shared/ui/toolbar";export function MarkdownToolbar() {  return (    <div className="flex justify-center">      <Toolbar>        <ToolbarBlock>          <MarkdownUndo />          <MarkdownRedo />        </ToolbarBlock>        <ToolbarBlock>          <MarkdownHeading />          <MarkdownList />          <MarkdownBlockquote />          <MarkdownCodeBlock />          <MarkdownImage />        </ToolbarBlock>        <ToolbarBlock>          <MarkdownBold />          <MarkdownItalic />          <MarkdownUnderline />          <MarkdownStrike />          <MarkdownCodeInline />          <MarkdownHighlight />          <MarkdownLink />        </ToolbarBlock>        <ToolbarBlock>          <MarkdownAlignLeft />          <MarkdownAlignCenter />          <MarkdownAlignRight />          <MarkdownAlignJustify />        </ToolbarBlock>      </Toolbar>    </div>  );}
