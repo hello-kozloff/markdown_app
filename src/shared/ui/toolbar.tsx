9d0870638027b8e49bf9f7b80058cc911dbfca15
Nikita <hello.kozloff@gmail.com>
2025-08-21 17:04:21 +0300

    support sticky toolbar

--- src/shared/ui/toolbar.tsx
+++ src/shared/ui/toolbar.tsx
@@ -1,95 +1,1 @@
-import { ButtonHTMLAttributes, ElementType, PropsWithChildren } from "react";
-import { cn } from "@/shared/lib/utils";
-import { PropsWithClassName } from "@/shared/types/utils";
-
-export type ToolbarProps =
-  PropsWithClassName<
-    PropsWithChildren<{
-      sticky?: boolean;
-    }>
-  >;
-
-export type ToolbarGroupProps =
-  PropsWithClassName<PropsWithChildren>;
-
-export type ToolbarItemProps<
-  T extends ElementType
-> =
-  ButtonHTMLAttributes<HTMLButtonElement> &
-    PropsWithChildren<
-      PropsWithClassName<{
-        as?: T;
-        isActive?: boolean;
-      }>
-    >;
-
-export function Toolbar({
-  sticky,
-  className,
-  children
-}: ToolbarProps) {
-  return (
-    <div
-      className={cn(
-        className,
-        "flex border-b bg-background",
-        {
-          "sticky top-0": sticky
-        }
-      )}
-    >
-      <div className="mx-auto p-2 flex">
-        {children}
-      </div>
-    </div>
-  );
-}
-
-export function ToolbarGroup({
-  className,
-  children
-}: ToolbarGroupProps) {
-  return (
-    <div
-      className={cn(
-        "px-2 flex gap-0.5 border-r last:border-r-0",
-        className
-      )}
-    >
-      {children}
-    </div>
-  );
-}
-
-export function ToolbarItem<
-  T extends ElementType = "button"
->({
-  as,
-  children,
-  isActive,
-  className,
-  ...props
-}: ToolbarItemProps<T>) {
-  const Component = as || "button";
-  return (
-    <Component
-      {...props}
-      className={cn(
-        className,
-        "p-2.5",
-        "flex gap-0.5 items-center",
-        "not-disabled:cursor-pointer transition ease-in-out rounded-full outline-0",
-        {
-          "text-gray-400 hover:not-disabled:text-white hover:not-disabled:bg-white/5":
-            !isActive,
-          "text-blue-500 bg-blue-500/10 hover:bg-blue-500/15":
-            isActive,
-          "text-gray-400/40":
-            props.disabled
-        }
-      )}
-    >
-      {children}
-    </Component>
-  );
-}
+import {  ButtonHTMLAttributes,  ElementType,  PropsWithChildren} from "react";import { cn } from "@/shared/lib/utils";import { PropsWithClassName } from "@/shared/types/utils";export type ToolbarProps =  PropsWithClassName<    PropsWithChildren<{      sticky?: boolean;    }>  >;export type ToolbarGroupProps =  PropsWithClassName<PropsWithChildren>;export type ToolbarItemProps<  T extends ElementType> =  ButtonHTMLAttributes<HTMLButtonElement> &    PropsWithChildren<      PropsWithClassName<{        as?: T;        isActive?: boolean;      }>    >;export function Toolbar({  sticky,  className,  children}: ToolbarProps) {  return (    <div      className={cn(        className,        "flex border-b bg-background",        {          "sticky top-0": sticky        }      )}    >      <div className="mx-auto p-2 flex">        {children}      </div>    </div>  );}export function ToolbarGroup({  className,  children}: ToolbarGroupProps) {  return (    <div      className={cn(        "px-2 flex gap-0.5 border-r last:border-r-0",        className      )}    >      {children}    </div>  );}export function ToolbarItem<  T extends ElementType = "button">({  as,  children,  isActive,  className,  ...props}: ToolbarItemProps<T>) {  const Component = as || "button";  return (    <Component      {...props}      className={cn(        className,        "p-2.5",        "flex gap-0.5 items-center",        "not-disabled:cursor-pointer transition ease-in-out rounded-full outline-0",        {          "text-gray-400 hover:not-disabled:text-white hover:not-disabled:bg-white/5":            !isActive,          "text-blue-500 bg-blue-500/10 hover:bg-blue-500/15":            isActive,          "text-gray-400/40":            props.disabled        }      )}    >      {children}    </Component>  );}
