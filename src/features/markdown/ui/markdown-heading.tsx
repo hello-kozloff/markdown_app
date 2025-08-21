d577e361d9e3c6b5d0b962da164b678d40c9db33
Nikita <hello.kozloff@gmail.com>
2025-08-21 19:34:12 +0300

    add tools config

--- src/features/markdown/ui/markdown-heading.tsx
+++ src/features/markdown/ui/markdown-heading.tsx
@@ -1,41 +1,1 @@
-import { useTranslations } from "next-intl";
-import { ChevronDownIcon, HeadingIcon } from "lucide-react";
-import { tools, useMarkdownNode } from "@/entities/markdown";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/shared/ui/dropdown-menu";
-import { ToolbarItem } from "@/shared/ui/toolbar";
-
-export function MarkdownHeading() {
-  const t = useTranslations("tools");
-  const markdownNode = useMarkdownNode({
-    node: "heading"
-  });
-
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <ToolbarItem
-          as="div"
-          isActive={markdownNode.isActive()}
-        >
-          <HeadingIcon size={16} />
-          <ChevronDownIcon size={10} />
-        </ToolbarItem>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        {tools.headings.map(
-          (heading, index) => (
-            <DropdownMenuItem
-              key={heading.name}
-              disabled={markdownNode.isActive(
-                { level: index + 1 }
-              )}
-            >
-              <heading.icon size={16} />
-              {t(heading.name)}
-            </DropdownMenuItem>
-          )
-        )}
-      </DropdownMenuContent>
-    </DropdownMenu>
-  );
-}
+import { useTranslations } from "next-intl";import {  ChevronDownIcon,  HeadingIcon} from "lucide-react";import {  tools,  useMarkdownNode} from "@/entities/markdown";import {  DropdownMenu,  DropdownMenuContent,  DropdownMenuItem,  DropdownMenuTrigger} from "@/shared/ui/dropdown-menu";import { ToolbarItem } from "@/shared/ui/toolbar";export function MarkdownHeading() {  const t = useTranslations("tools");  const markdownNode = useMarkdownNode({    node: "heading"  });  return (    <DropdownMenu>      <DropdownMenuTrigger>        <ToolbarItem          as="div"          isActive={markdownNode.isActive()}        >          <HeadingIcon size={16} />          <ChevronDownIcon size={10} />        </ToolbarItem>      </DropdownMenuTrigger>      <DropdownMenuContent>        {tools.headings.map(          (heading, index) => (            <DropdownMenuItem              key={heading.name}              disabled={markdownNode.isActive(                { level: index + 1 }              )}            >              <heading.icon size={16} />              {t(heading.name)}            </DropdownMenuItem>          )        )}      </DropdownMenuContent>    </DropdownMenu>  );}
