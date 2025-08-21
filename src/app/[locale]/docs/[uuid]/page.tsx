7618ce2c965038a303689e9bc659628ac578f4d0
Nikita <hello.kozloff@gmail.com>
2025-08-21 15:30:25 +0300

    support underline button

--- src/app/[locale]/docs/[uuid]/page.tsx
+++ src/app/[locale]/docs/[uuid]/page.tsx
@@ -1,40 +1,1 @@
-import { MarkdownContent, MarkdownContext } from "@/entities/markdown";
-import { MarkdownToolbar } from "@/widgets/markdown";
-
-const content = `
- 
-  # Getting started
-
-  Welcome to the *Simple Editor* template! This template integrates open source UI components and Tiptap extensions licensed under MIT.
-
-  Integrate it by following the [Tiptap UI Components docs](https://duckduckgo.com "Tiptap UI Components docs") or using our CLI tool.
-
-  ## Features
-
-  > A fully responsive rich text editor with built-in support for common formatting and layout tools. Type markdown \`**\` or use keyboard shortcuts \`⌘+B\` for most all common markdown marks.
-
-  Add images, customize alignment, and apply advanced formatting to make your writing more engaging and professional.
-
-  ![The San Juan Mountains are beautiful!](https://mdg.imgix.net/assets/images/san-juan-mountains.jpg?auto=format&fit=clip&q=40&w=1080 "San Juan Mountains")
-  
-  1. Superscript (x2) and Subscript (H2O) for precision.
-  2. Typographic conversion: automatically convert to \`->\` an arrow →.
-  
-  ***
-  
-  ## Make it your own
-  
-  Switch between light and dark modes, and tailor the editor's appearance with customizable CSS to match your style.
-  
-`;
-
-export default function DocumentPage() {
-  return (
-    <MarkdownContext
-      initialMarkdown={content}
-    >
-      <MarkdownToolbar />
-      <MarkdownContent />
-    </MarkdownContext>
-  );
-}
+import {  MarkdownContent,  MarkdownContext} from "@/entities/markdown";import { MarkdownToolbar } from "@/widgets/markdown";const content = `   # Getting started  Welcome to the *Simple Editor* template! This template integrates open source UI components and Tiptap extensions licensed under MIT.  Integrate it by following the [Tiptap UI Components docs](https://duckduckgo.com "Tiptap UI Components docs") or using our CLI tool.  ## Features  > A fully responsive rich text editor with built-in support for common formatting and layout tools. Type markdown \`**\` or use keyboard shortcuts \`⌘+B\` for most all common markdown marks.  Add images, customize alignment, and apply advanced formatting to make your writing more engaging and professional.  ![The San Juan Mountains are beautiful!](https://mdg.imgix.net/assets/images/san-juan-mountains.jpg?auto=format&fit=clip&q=40&w=1080 "San Juan Mountains")    1. Superscript (x2) and Subscript (H2O) for precision.  2. Typographic conversion: automatically convert to \`->\` an arrow →.    ***    ## Make it your own    Switch between light and dark modes, and tailor the editor's appearance with customizable CSS to match your style.  `;export default function DocumentPage() {  return (    <MarkdownContext      initialMarkdown={content}    >      <MarkdownToolbar />      <MarkdownContent />    </MarkdownContext>  );}
