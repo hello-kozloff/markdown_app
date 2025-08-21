4bc2567f3292ef0bb0cd2653d95f9caaf0548e1f
Nikita <hello.kozloff@gmail.com>
2025-08-21 15:20:54 +0300

    support bold button

--- src/features/markdown/ui/markdown-bold.tsx
+++ src/features/markdown/ui/markdown-bold.tsx
@@ -1,9 +1,40 @@
 import { BoldIcon } from "lucide-react";
+import {
+  useEditorEventCallback,
+  useEditorState
+} from "@handlewithcare/react-prosemirror";
+import { toggleMark } from "prosemirror-commands";
+import { isMarkActive } from "@/shared/lib/utils";
 import { ToolbarItem } from "@/shared/ui/toolbar";
 
 export function MarkdownBold() {
+  const state = useEditorState();
+
+  const toggleBold =
+    useEditorEventCallback((view) => {
+      const toggleBoldMark = toggleMark(
+        view.state.schema.marks[
+          "strong"
+        ]
+      );
+
+      toggleBoldMark(
+        view.state,
+        view.dispatch,
+        view
+      );
+
+      view.focus();
+    });
+
   return (
-    <ToolbarItem>
+    <ToolbarItem
+      onClick={toggleBold}
+      isActive={isMarkActive(
+        state.schema.marks["strong"],
+        state
+      )}
+    >
       <BoldIcon size={16} />
     </ToolbarItem>
   );
