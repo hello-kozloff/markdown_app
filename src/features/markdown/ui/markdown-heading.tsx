af1102e2adb9d2e4a1002f1ee0d1b99c7cb92e07
Nikita <hello.kozloff@gmail.com>
2025-08-21 17:48:21 +0300

    update heading button

--- src/features/markdown/ui/markdown-heading.tsx
+++ src/features/markdown/ui/markdown-heading.tsx
@@ -1,14 +1,54 @@
 import {
   ChevronDownIcon,
+  Heading1Icon,
+  Heading2Icon,
+  Heading3Icon,
+  Heading4Icon,
+  Heading5Icon,
+  Heading6Icon,
   HeadingIcon
 } from "lucide-react";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/shared/ui/dropdown-menu";
 import { ToolbarItem } from "@/shared/ui/toolbar";
+import { useTranslations } from "next-intl";
 
 export function MarkdownHeading() {
+  const t = useTranslations("tools");
+
   return (
-    <ToolbarItem>
-      <HeadingIcon size={16} />
-      <ChevronDownIcon size={10} />
-    </ToolbarItem>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <ToolbarItem as="div">
+          <HeadingIcon size={16} />
+          <ChevronDownIcon size={10} />
+        </ToolbarItem>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem>
+          <Heading1Icon />
+          {t("heading-1")}
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Heading2Icon />
+          {t("heading-2")}
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Heading3Icon />
+          {t("heading-3")}
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Heading4Icon />
+          {t("heading-4")}
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Heading5Icon />
+          {t("heading-5")}
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Heading6Icon />
+          {t("heading-6")}
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
