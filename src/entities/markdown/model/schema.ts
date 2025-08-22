d41e0c8f73be7b89db47f30207c23abbcbec2731
Nikita <hello.kozloff@gmail.com>
2025-08-22 11:49:47 +0300

    update schema

--- src/entities/markdown/model/schema.ts
+++ src/entities/markdown/model/schema.ts
@@ -3,29 +3,54 @@ import { Schema } from "prosemirror-model";
 export const schema = new Schema({
   nodes: {
     doc: { content: "block+" },
+
     paragraph: {
       group: "block",
       content: "inline*",
       toDOM() {
         return ["p", 0];
-      }
+      },
+      parseDOM: [{ tag: "p" }]
     },
+
     heading: {
       group: "block",
       content: "inline*",
-      attrs: {
-        level: {
-          default: 1,
-          validate: "number"
-        }
-      },
+      attrs: { level: { default: 1 } },
       toDOM(node) {
         return [
           `h${node.attrs.level}`,
           0
         ];
-      }
+      },
+      parseDOM: [
+        {
+          tag: "h1",
+          getAttrs: () => ({ level: 1 })
+        },
+        {
+          tag: "h2",
+          getAttrs: () => ({ level: 2 })
+        },
+        {
+          tag: "h3",
+          getAttrs: () => ({ level: 3 })
+        },
+        {
+          tag: "h4",
+          getAttrs: () => ({ level: 4 })
+        },
+        {
+          tag: "h5",
+          getAttrs: () => ({ level: 5 })
+        },
+        {
+          tag: "h6",
+          getAttrs: () => ({ level: 6 })
+        }
+      ]
     },
+
     image: {
       inline: true,
       group: "inline",
@@ -44,8 +69,32 @@ export const schema = new Schema({
             title: node.attrs.title
           }
         ];
-      }
+      },
+      parseDOM: [
+        {
+          tag: "img[src]",
+          getAttrs: (dom) => {
+            const el =
+              dom as HTMLImageElement;
+            return {
+              src:
+                el.getAttribute(
+                  "src"
+                ) ?? "",
+              alt:
+                el.getAttribute(
+                  "alt"
+                ) ?? "",
+              title:
+                el.getAttribute(
+                  "title"
+                ) ?? ""
+            };
+          }
+        }
+      ]
     },
+
     blockquote: {
       content: "block+",
       group: "block",
@@ -55,7 +104,8 @@ export const schema = new Schema({
       },
       parseDOM: [{ tag: "blockquote" }]
     },
-    list: {
+
+    ordered_list: {
       group: "block",
       content: "list_item+",
       toDOM() {
@@ -64,59 +114,91 @@ export const schema = new Schema({
           { class: "list-decimal" },
           0
         ];
-      }
+      },
+      parseDOM: [{ tag: "ol" }]
     },
+
+    bullet_list: {
+      group: "block",
+      content: "list_item+",
+      toDOM() {
+        return [
+          "ul",
+          { class: "list-disc" },
+          0
+        ];
+      },
+      parseDOM: [{ tag: "ul" }]
+    },
+
     list_item: {
       content: "paragraph+",
       toDOM() {
         return ["li", 0];
-      }
+      },
+      parseDOM: [{ tag: "li" }]
     },
+
     text: { group: "inline" }
   },
+
   marks: {
     em: {
       toDOM() {
-        return ["em"];
+        return ["em", 0];
       },
       parseDOM: [
-        {
-          tag: "em"
-        }
+        { tag: "em" },
+        { tag: "i" },
+        { style: "font-style=italic" }
       ]
     },
+
     strong: {
       toDOM() {
-        return ["strong"];
+        return ["strong", 0];
       },
       parseDOM: [
-        {
-          tag: "strong"
-        }
+        { tag: "strong" },
+        { tag: "b" },
+        { style: "font-weight=bold" }
       ]
     },
+
     underline: {
       toDOM() {
         return ["u", 0];
       },
-      parseDOM: [{ tag: "u" }]
+      parseDOM: [
+        { tag: "u" },
+        {
+          style:
+            "text-decoration=underline"
+        }
+      ]
     },
+
     strike: {
       toDOM() {
         return ["s", 0];
       },
-      parseDOM: [{ tag: "s" }]
-    },
-    code: {
-      toDOM() {
-        return ["code"];
-      },
       parseDOM: [
+        { tag: "s" },
+        { tag: "del" },
         {
-          tag: "code"
+          style:
+            "text-decoration=line-through"
         }
       ]
     },
+
+    code: {
+      toDOM() {
+        return ["code", 0];
+      },
+      parseDOM: [{ tag: "code" }]
+    },
+
     highlight: {
       toDOM() {
         return [
@@ -133,27 +215,35 @@ export const schema = new Schema({
         }
       ]
     },
+
     link: {
       attrs: {
-        url: {
-          default: "",
-          validate: "string"
-        }
+        href: { default: "" },
+        title: { default: null }
       },
+      inclusive: false,
       toDOM(mark) {
         return [
           "a",
-          { href: mark.attrs.url }
+          {
+            href: mark.attrs.href,
+            title: mark.attrs.title
+          },
+          0
         ];
       },
       parseDOM: [
         {
-          tag: "a",
-          getAttrs(node) {
+          tag: "a[href]",
+          getAttrs(dom) {
+            const el =
+              dom as HTMLAnchorElement;
             return {
-              url: (
-                node as HTMLAnchorElement
-              ).href
+              href: el.getAttribute(
+                "href"
+              ),
+              title:
+                el.getAttribute("title")
             };
           }
         }
