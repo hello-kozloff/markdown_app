3e3b39ff7203197032b671cdaa59b6cc9e65d9ed
Nikita <hello.kozloff@gmail.com>
2025-08-27 13:47:53 +0300

    decompose toolbar

--- src/dev/markdown.tsx
+++ src/dev/markdown.tsx
@@ -1,28 +1,1 @@
-import { JSX } from "react";
-import { MarkdownToolbar } from "./markdown-toolbar";
-import { MarkdownContent, MarkdownContext } from "@/entities/markdown";
-
-export interface MarkdownProps {
-  name: string;
-  data: string;
-  toolbar?: (() => JSX.Element)[][];
-  disabled?: boolean;
-  readonly?: boolean;
-}
-export function Markdown({
-  name,
-  data,
-  toolbar
-}: MarkdownProps) {
-  return (
-    <MarkdownContext data={data}>
-      {toolbar && (
-        <MarkdownToolbar
-          name={name}
-          toolbar={toolbar}
-        />
-      )}
-      <MarkdownContent />
-    </MarkdownContext>
-  );
-}
+import { JSX } from "react";import { MarkdownToolbar } from "./markdown-toolbar";import {  MarkdownContent,  MarkdownContext} from "@/entities/markdown";export interface MarkdownProps {  name: string;  data: string;  toolbar?: (() => JSX.Element)[][];  disabled?: boolean;  readonly?: boolean;}export function Markdown({  name,  data,  toolbar}: MarkdownProps) {  return (    <MarkdownContext data={data}>      {toolbar && (        <MarkdownToolbar          name={name}          toolbar={toolbar}        />      )}      <MarkdownContent />    </MarkdownContext>  );}
