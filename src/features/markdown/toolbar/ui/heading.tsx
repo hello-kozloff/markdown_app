985922c68311e6e311bd5caef9533d256a7f914d
Nikita <hello.kozloff@gmail.com>
2025-08-29 01:45:37 +0300

    update

--- src/features/markdown/toolbar/ui/heading.tsx
+++ src/features/markdown/toolbar/ui/heading.tsx
@@ -9,10 +9,7 @@ import {
   Heading6Icon,
   HeadingIcon
 } from "lucide-react";
-import {
-  MarkdownToolbarItem,
-  useMarkdown
-} from "@/entities/markdown";
+import { MarkdownToolbarItem, useMarkdown } from "@/entities/markdown";
 
 export function Heading() {
   const markdown = useMarkdown();
@@ -29,26 +26,46 @@ export function Heading() {
     >
       {Array.from({
         length: 6
-      }).map((_, index) => ({
-        name: `heading-${index + 1}`,
-        icon: [
+      }).map((_, index) => {
+        const name = `heading-${index + 1}`;
+
+        const icon = [
           Heading1Icon,
           Heading2Icon,
           Heading3Icon,
           Heading4Icon,
           Heading5Icon,
           Heading6Icon
-        ][index],
-        isActive: markdown.isNodeActive(
-          nodeType,
-          { level: index + 1 }
-        ),
-        onClick: () =>
-          markdown.setBlockType(
+        ][index];
+
+        const isActive =
+          markdown.isNodeActive(
             nodeType,
             { level: index + 1 }
-          )
-      }))}
+          );
+
+        const onClick = () => {
+          if (isActive) {
+            return markdown.setBlockType(
+              markdown.getNodeType(
+                "paragraph"
+              )
+            );
+          }
+
+          return markdown.setBlockType(
+            nodeType,
+            { level: index + 1 }
+          );
+        };
+
+        return {
+          name,
+          icon,
+          isActive,
+          onClick
+        };
+      })}
     </MarkdownToolbarItem>
   );
 }
