b8d541da58dc04d83440d45f5b8a95eefc639819
Nikita <hello.kozloff@gmail.com>
2025-08-28 20:11:40 +0300

    update

--- src/entities/markdown/ui/markdown-toolbar-item.tsx
+++ src/entities/markdown/ui/markdown-toolbar-item.tsx
@@ -1,86 +1,1 @@
-"use client";
-
-import { ForwardRefExoticComponent } from "react";
-import { ChevronDownIcon, LucideProps } from "lucide-react";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/shared/ui/dropdown-menu";
-import { ToolbarItem } from "@/shared/ui/toolbar";
-import { useTranslations } from "next-intl";
-import { cn } from "@/shared/lib/utils";
-
-export interface MarkdownToolbarItemProps {
-  name: string;
-  icon: ForwardRefExoticComponent<
-    Omit<LucideProps, "ref">
-  >;
-  children?: Omit<
-    MarkdownToolbarItemProps,
-    "children"
-  >[];
-  isActive?: boolean;
-  isDisabled?: boolean;
-  onClick?: () => void;
-}
-
-export function MarkdownToolbarItem({
-  icon: IconComponent,
-  isActive,
-  isDisabled,
-  children,
-  onClick
-}: MarkdownToolbarItemProps) {
-  const t = useTranslations("toolbar");
-
-  if (!!children) {
-    return (
-      <DropdownMenu>
-        <DropdownMenuTrigger
-          asChild
-          className="rounded-full"
-        >
-          <ToolbarItem
-            as="div"
-            isActive={isActive}
-            disabled={isDisabled}
-            className={cn(
-              isActive
-                ? "data-[state=open]:bg-blue-500/15 data-[state=open]:border-border data-[state=open]:text-blue-500"
-                : "data-[state=open]:bg-white/5 data-[state=open]:text-white"
-            )}
-          >
-            <IconComponent size={16} />
-            <ChevronDownIcon
-              size={10}
-            />
-          </ToolbarItem>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          {children.map(
-            (toolbarItem) => (
-              <DropdownMenuItem
-                key={toolbarItem.name}
-                onClick={
-                  toolbarItem.onClick
-                }
-              >
-                <toolbarItem.icon
-                  size={16}
-                />
-                {t(toolbarItem.name)}
-              </DropdownMenuItem>
-            )
-          )}
-        </DropdownMenuContent>
-      </DropdownMenu>
-    );
-  }
-
-  return (
-    <ToolbarItem
-      isActive={isActive}
-      disabled={isDisabled}
-      onClick={onClick}
-    >
-      <IconComponent size={16} />
-    </ToolbarItem>
-  );
-}
+"use client";import { ForwardRefExoticComponent } from "react";import {  ChevronDownIcon,  LucideProps} from "lucide-react";import {  DropdownMenu,  DropdownMenuContent,  DropdownMenuItem,  DropdownMenuTrigger} from "@/shared/ui/dropdown-menu";import { ToolbarItem } from "@/shared/ui/toolbar";import { useTranslations } from "next-intl";import { cn } from "@/shared/lib/utils";export interface MarkdownToolbarItemProps {  name: string;  icon: ForwardRefExoticComponent<    Omit<LucideProps, "ref">  >;  children?: Omit<    MarkdownToolbarItemProps,    "children"  >[];  isActive?: boolean;  isDisabled?: boolean;  onClick?: () => void;}export function MarkdownToolbarItem({  icon: IconComponent,  isActive,  isDisabled,  children,  onClick}: MarkdownToolbarItemProps) {  const t = useTranslations("toolbar");  if (!!children) {    return (      <DropdownMenu>        <DropdownMenuTrigger          asChild          className="rounded-full"        >          <ToolbarItem            as="div"            isActive={isActive}            disabled={isDisabled}            className={cn(              isActive                ? "data-[state=open]:bg-blue-500/15 data-[state=open]:border-border data-[state=open]:text-blue-500"                : "data-[state=open]:bg-white/5 data-[state=open]:text-white"            )}          >            <IconComponent size={16} />            <ChevronDownIcon              size={10}            />          </ToolbarItem>        </DropdownMenuTrigger>        <DropdownMenuContent>          {children.map(            (toolbarItem) => (              <DropdownMenuItem                key={toolbarItem.name}                onClick={                  toolbarItem.onClick                }              >                <toolbarItem.icon                  size={16}                />                {t(toolbarItem.name)}              </DropdownMenuItem>            )          )}        </DropdownMenuContent>      </DropdownMenu>    );  }  return (    <ToolbarItem      isActive={isActive}      disabled={isDisabled}      onClick={onClick}    >      <IconComponent size={16} />    </ToolbarItem>  );}
