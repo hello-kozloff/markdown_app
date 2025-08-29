eef741c059464f2ef59c21f412457036e313581a
Nikita <hello.kozloff@gmail.com>
2025-08-29 15:32:19 +0300

    update

--- src/features/markdown/toolbar/ui/heading.tsx
+++ src/features/markdown/toolbar/ui/heading.tsx
@@ -1,73 +1,1 @@
-"use client";
-
-import {
-  Heading1Icon,
-  Heading2Icon,
-  Heading3Icon,
-  Heading4Icon,
-  Heading5Icon,
-  Heading6Icon,
-  HeadingIcon
-} from "lucide-react";
-import { MarkdownToolbarItem, useMarkdown } from "@/entities/markdown";
-
-export const icons = [
-  Heading1Icon,
-  Heading2Icon,
-  Heading3Icon,
-  Heading4Icon,
-  Heading5Icon,
-  Heading6Icon
-];
-
-export function Heading() {
-  const markdown = useMarkdown();
-  const nodeType =
-    markdown.getNodeType("heading");
-
-  return (
-    <MarkdownToolbarItem
-      name="heading"
-      icon={HeadingIcon}
-      isActive={markdown.isNodeActive(
-        nodeType
-      )}
-    >
-      {Array.from({
-        length: 6
-      }).map((_, index) => {
-        const level = index + 1;
-        const name = `heading-${level}`;
-        const icon = icons[index];
-
-        const isActive =
-          markdown.isNodeActive(
-            nodeType,
-            { level }
-          );
-
-        const onClick = () => {
-          if (isActive) {
-            return markdown.setBlockType(
-              markdown.getNodeType(
-                "paragraph"
-              )
-            );
-          }
-
-          return markdown.setBlockType(
-            nodeType,
-            { level }
-          );
-        };
-
-        return {
-          name,
-          icon,
-          isActive,
-          onClick
-        };
-      })}
-    </MarkdownToolbarItem>
-  );
-}
+"use client";import {  Heading1Icon,  Heading2Icon,  Heading3Icon,  Heading4Icon,  Heading5Icon,  Heading6Icon,  HeadingIcon} from "lucide-react";import {  MarkdownToolbarItem,  useMarkdown} from "@/entities/markdown";export const icons = [  Heading1Icon,  Heading2Icon,  Heading3Icon,  Heading4Icon,  Heading5Icon,  Heading6Icon];export function Heading() {  const markdown = useMarkdown();  const nodeType =    markdown.getNodeType("heading");  return (    <MarkdownToolbarItem      name="heading"      icon={HeadingIcon}      isActive={markdown.isNodeActive(        nodeType      )}    >      {Array.from({        length: 6      }).map((_, index) => {        const level = index + 1;        const name = `heading-${level}`;        const icon = icons[index];        const isActive =          markdown.isNodeActive(            nodeType,            { level }          );        const onClick = () => {          if (isActive) {            return markdown.setBlockType(              markdown.getNodeType(                "paragraph"              )            );          }          return markdown.setBlockType(            nodeType,            { level }          );        };        return {          name,          icon,          isActive,          onClick        };      })}    </MarkdownToolbarItem>  );}
